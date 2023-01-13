c0d4e93464a8108c3788f583156a07aa4a58c81c
JEETDESAI25 <jeetdesai25@gmail.com>
2023-01-12 19:02:41 -0800

    Fixed KeyError and temp value in graph

--- main.py
+++ main.py
@@ -12,25 +12,28 @@ option = st.selectbox("Select Data to View", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place} ")
 
 if place:
-    # Get the temperature/Sky data
-    filtered_data = get_data(place, days)
+    try:
+        # Get the temperature/Sky data
+        filtered_data = get_data(place, days)
 
-    if option == "Temperature":
-        # Create the temperature plot
-        temperatures = [dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        figure = px.line(
-            x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"}
-        )
-        st.plotly_chart(figure)
+        if option == "Temperature":
+            # Create the temperature plot
+            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(
+                x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"}
+            )
+            st.plotly_chart(figure)
 
-    if option == "Sky":
-        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
-        images = {
-            "Clear": "images/clear.png",
-            "Clouds": "images/cloud.png",
-            "Rain": "images/rain.png",
-            "Snow": "iamges/snow.png",
-        }
-        image_path = [images[condition] for condition in sky_conditions]
-        st.image(image_path, width=115)
+        if option == "Sky":
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            images = {
+                "Clear": "images/clear.png",
+                "Clouds": "images/cloud.png",
+                "Rain": "images/rain.png",
+                "Snow": "iamges/snow.png",
+            }
+            image_path = [images[condition] for condition in sky_conditions]
+            st.image(image_path, width=115)
+    except KeyError:
+        st.info("This place data is not available. Please enter the different name.")
